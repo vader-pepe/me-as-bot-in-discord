231f0e540ca44959cc56470d84adc75a049554da
vader-pepe <vader-pepe@email.com>
2024-12-28 09:13:15 +0700

    feat: initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
-#!/usr/bin/env node
-// NOTE: You can remove the first line if you don't plan to release an
-// executable package. E.g. code that can be used as cli like prettier or eslint
+import { Client } from 'discord.js-selfbot-v13';
+import 'dotenv/config'
 
-const main = () => {
-  console.log("hello Node.js and Typescript world :]");
-  console.log("live reloading");
-  console.log("live reloading2");
-};
+const client = new Client();
+const token = process.env.TOKEN;
 
-// This was just here to force a linting error for now to demonstrate/test the
-// eslint pipeline. You can uncomment this and run "yarn lint:check" to test the
-// linting.
-// const x: number[] = [1, 2];
-// const y: Array<number> = [3, 4];
-// if (x == y) {
-//   console.log("equal!");
-// }
+client.on('ready', async () => {
+  if (client.user) {
+    console.log(`${client.user.username} is ready!`);
+  }
+})
 
-main();
+client.login(token);
+
